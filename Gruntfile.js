43a84b8c056da2207fafcf51a18e943daff9010e
Torrey Rice <rice@sitepen.com>
2018-03-10 07:19:04 -0800

    Adding initial mayhem monsters and custom theme file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,66 +1,66 @@
 /* global module:false */
 module.exports = function(grunt) {
-	var port = grunt.option('port') || 8000;
-	var root = grunt.option('root') || '.';
+	var port = grunt.option("port") || 8000;
+	var root = grunt.option("root") || ".";
 
 	if (!Array.isArray(root)) root = [root];
 
 	// Project configuration
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+		pkg: grunt.file.readJSON("package.json"),
 		meta: {
 			banner:
-				'/*!\n' +
+				"/*!\n" +
 				' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-				' * http://revealjs.com\n' +
-				' * MIT licensed\n' +
-				' *\n' +
-				' * Copyright (C) 2017 Hakim El Hattab, http://hakim.se\n' +
-				' */'
+				" * http://revealjs.com\n" +
+				" * MIT licensed\n" +
+				" *\n" +
+				" * Copyright (C) 2017 Hakim El Hattab, http://hakim.se\n" +
+				" */"
 		},
 
 		qunit: {
-			files: [ 'test/*.html' ]
+			files: ["test/*.html"]
 		},
 
 		uglify: {
 			options: {
-				banner: '<%= meta.banner %>\n',
+				banner: "<%= meta.banner %>\n",
 				screwIE8: false
 			},
 			build: {
-				src: 'js/reveal.js',
-				dest: 'js/reveal.min.js'
+				src: "js/reveal.js",
+				dest: "js/reveal.min.js"
 			}
 		},
 
 		sass: {
 			core: {
-				src: 'css/reveal.scss',
-				dest: 'css/reveal.css'
+				src: "css/reveal.scss",
+				dest: "css/reveal.css"
 			},
 			themes: {
 				expand: true,
-				cwd: 'css/theme/source',
-				src: ['*.sass', '*.scss'],
-				dest: 'css/theme',
-				ext: '.css'
+				cwd: "css/theme/source",
+				src: ["*.sass", "*.scss"],
+				dest: "css/theme",
+				ext: ".css"
 			}
 		},
 
 		autoprefixer: {
 			core: {
-				src: 'css/reveal.css'
+				src: "css/reveal.css"
 			}
 		},
 
 		cssmin: {
 			options: {
-				compatibility: 'ie9'
+				compatibility: "ie9"
 			},
 			compress: {
-				src: 'css/reveal.css',
-				dest: 'css/reveal.min.css'
+				src: "css/reveal.css",
+				dest: "css/reveal.min.css"
 			}
 		},
 
@@ -70,7 +70,7 @@ module.exports = function(grunt) {
 				eqeqeq: true,
 				immed: true,
 				esnext: true,
-				latedef: 'nofunc',
+				latedef: "nofunc",
 				newcap: true,
 				noarg: true,
 				sub: true,
@@ -87,7 +87,7 @@ module.exports = function(grunt) {
 					exports: false
 				}
 			},
-			files: [ 'Gruntfile.js', 'js/reveal.js' ]
+			files: ["Gruntfile.js", "js/reveal.js"]
 		},
 
 		connect: {
@@ -105,41 +105,41 @@ module.exports = function(grunt) {
 		zip: {
 			bundle: {
 				src: [
-					'index.html',
-					'css/**',
-					'js/**',
-					'lib/**',
-					'images/**',
-					'plugin/**',
-					'**.md'
+					"index.html",
+					"css/**",
+					"js/**",
+					"lib/**",
+					"images/**",
+					"plugin/**",
+					"**.md"
 				],
-				dest: 'reveal-js-presentation.zip'
+				dest: "reveal-js-presentation.zip"
 			}
 		},
 
 		watch: {
 			js: {
-				files: [ 'Gruntfile.js', 'js/reveal.js' ],
-				tasks: 'js'
+				files: ["Gruntfile.js", "js/reveal.js"],
+				tasks: "js"
 			},
 			theme: {
 				files: [
-					'css/theme/source/*.sass',
-					'css/theme/source/*.scss',
-					'css/theme/template/*.sass',
-					'css/theme/template/*.scss'
+					"css/theme/source/*.sass",
+					"css/theme/source/*.scss",
+					"css/theme/template/*.sass",
+					"css/theme/template/*.scss"
 				],
-				tasks: 'css-themes'
+				tasks: "css-themes"
 			},
 			css: {
-				files: [ 'css/reveal.scss' ],
-				tasks: 'css-core'
+				files: ["css/reveal.scss"],
+				tasks: "css-core"
 			},
 			html: {
-				files: root.map(path => path + '/*.html')
+				files: root.map(path => path + "/*.html")
 			},
 			markdown: {
-				files: root.map(path => path + '/*.md')
+				files: root.map(path => path + "/*.md")
 			},
 			options: {
 				livereload: true
@@ -147,46 +147,44 @@ module.exports = function(grunt) {
 		},
 
 		retire: {
-			js: [ 'js/reveal.js', 'lib/js/*.js', 'plugin/**/*.js' ],
-			node: [ '.' ]
+			js: ["js/reveal.js", "lib/js/*.js", "plugin/**/*.js"],
+			node: ["."]
 		}
-
 	});
 
 	// Dependencies
-	grunt.loadNpmTasks( 'grunt-contrib-connect' );
-	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
-	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
-	grunt.loadNpmTasks( 'grunt-contrib-watch' );
-	grunt.loadNpmTasks( 'grunt-autoprefixer' );
-	grunt.loadNpmTasks( 'grunt-retire' );
-	grunt.loadNpmTasks( 'grunt-sass' );
-	grunt.loadNpmTasks( 'grunt-zip' );
-	
+	grunt.loadNpmTasks("grunt-contrib-connect");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-qunit");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-watch");
+	grunt.loadNpmTasks("grunt-autoprefixer");
+	grunt.loadNpmTasks("grunt-retire");
+	grunt.loadNpmTasks("grunt-sass");
+	grunt.loadNpmTasks("grunt-zip");
+
 	// Default task
-	grunt.registerTask( 'default', [ 'css', 'js' ] );
+	grunt.registerTask("default", ["css", "js"]);
 
 	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify', 'qunit' ] );
+	grunt.registerTask("js", ["jshint", "uglify", "qunit"]);
 
 	// Theme CSS
-	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
+	grunt.registerTask("css-themes", ["sass:themes"]);
 
 	// Core framework CSS
-	grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
+	grunt.registerTask("css-core", ["sass:core", "autoprefixer", "cssmin"]);
 
 	// All CSS
-	grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
+	grunt.registerTask("css", ["sass", "autoprefixer", "cssmin", "watch"]);
 
 	// Package presentation to archive
-	grunt.registerTask( 'package', [ 'default', 'zip' ] );
+	grunt.registerTask("package", ["default", "zip"]);
 
 	// Serve presentation locally
-	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
+	grunt.registerTask("serve", ["connect", "watch"]);
 
 	// Run tests
-	grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
-
+	grunt.registerTask("test", ["jshint", "qunit"]);
 };
